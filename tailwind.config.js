acdbc25f7ae446ca873fb9dd4015086d4e5aa4f6
John Wilker <johnwilker2@gmail.com>
2022-12-23 22:11:14 -0300

    Landing Page using TailWind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  darkMode: 'class',
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
